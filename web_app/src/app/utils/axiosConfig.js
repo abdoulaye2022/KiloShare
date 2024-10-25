93910c2e52db0f92c13346cce1c99e9b5a3fdefd
Abdoulaye Mohamed Ahmed <m2atodev@gmail.com>
2024-10-24 22:01:22 -0300

    deploy on vercel

--- web_app/src/app/utils/axiosConfig.js
+++ web_app/src/app/utils/axiosConfig.js
@@ -1,16 +1,8 @@
-// First we need to import axios.js
 import axios from 'axios';
-// import Cookies from "js-cookie";
 
-// Next we make an 'instance' of it
 const instance = axios.create({
-// .. where we make our configurations
-    baseURL: 'http://localhost/KiloShare'
+    // baseURL: 'http://localhost/KiloShare'
+    baseURL: 'http://m2acode.com'
 });
-
-// const jwtToken = Cookies.get("jwt");
-
-// Where you would set stuff like your 'Authorization' header, etc ...
-// instance.defaults.headers.common['Authorization'] = `Baerer ${jwtToken}`;
 
 export default instance;
