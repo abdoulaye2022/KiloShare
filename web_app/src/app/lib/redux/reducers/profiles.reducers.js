7e86b098372d4ed7af6eebd036d52f4864499aef
Abdoulaye Mohamed Ahmed <m2atodev@gmail.com>
2024-11-07 21:33:14 -0400

    deployement sur kilo-share

--- web_app/src/app/lib/redux/reducers/profiles.reducers.js
+++ web_app/src/app/lib/redux/reducers/profiles.reducers.js
@@ -29,21 +29,19 @@ export const profileSlice = createSlice({
     },
     // ADD
     requestAdd: (state = initialState, action) => {
-        action.type = profileConstants.ADD_PROFILES_REQUEST;
-        state.loading = true;
-      },
-      successAdd: (state = initialState, action) => {
-        action.type = profileConstants.ADD_PROFILES_SUCCESS;
-        state.loading = false;
-        state.items = [...state.items, action.payload];
-      },
-      failureAdd: (state = initialState, action) => {
-        action.type = profileConstants.ADD_PROFILES_FAILURE;
-        state.loading = false;
-        state.error = action.payload;
-      },
-  
-
+      action.type = profileConstants.ADD_PROFILES_REQUEST;
+      state.loading = true;
+    },
+    successAdd: (state = initialState, action) => {
+      action.type = profileConstants.ADD_PROFILES_SUCCESS;
+      state.loading = false;
+      state.items = [...state.items, action.payload];
+    },
+    failureAdd: (state = initialState, action) => {
+      action.type = profileConstants.ADD_PROFILES_FAILURE;
+      state.loading = false;
+      state.error = action.payload;
+    },
   },
 });
 
@@ -51,5 +49,8 @@ export const {
   requestGetAll,
   successGetAll,
   failureGetAll,
+  requestAdd,
+  successAdd,
+  failureAdd,
 } = profileSlice.actions;
 export const profileReducer = profileSlice.reducer;
