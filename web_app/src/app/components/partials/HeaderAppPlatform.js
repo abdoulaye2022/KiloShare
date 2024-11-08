bddcb0f787f79ad4927bb4f89b340e3310266c32
Abdoulaye Mohamed Ahmed <m2atodev@gmail.com>
2024-11-07 23:59:18 -0400

    Listening form

--- web_app/src/app/components/partials/HeaderAppPlatform.js
+++ web_app/src/app/components/partials/HeaderAppPlatform.js
@@ -39,52 +39,51 @@ const items = [
     key: "1",
     icon: <PlusCircleOutlined />,
     label: "Post a Listing",
-  }
+  },
 ];
 
-
 const items1 = [
-    {
-      key: "sub1",
-      label: "",
-      icon: (
-        <Avatar
-          style={{ backgroundColor: "white" }}
-          size="large"
-          icon={<UserOutlined style={{ color: "black" }} />}
-        />
-      ),
-      children: [
-        {
-          key: "2",
-          label: "Log In",
-        },
-        {
-          key: "3",
-          label: "Sign In",
-        },
-        {
-          key: "4",
-          label: "My Listings",
-        },
-        {
-          type: "divider",
-        },
-        {
-          key: "5",
-          label: "About us",
-        },
-        {
-          key: "6",
-          label: "Help Center",
-        },
-        {
-          key: "7",
-          label: "Settings",
-        },
-      ],
-    },
-  ];
+  {
+    key: "sub1",
+    label: "",
+    icon: (
+      <Avatar
+        style={{ backgroundColor: "white" }}
+        size="large"
+        icon={<UserOutlined style={{ color: "black" }} />}
+      />
+    ),
+    children: [
+      {
+        key: "2",
+        label: "Log In",
+      },
+      {
+        key: "3",
+        label: "Sign In",
+      },
+      {
+        key: "4",
+        label: "My Listings",
+      },
+      {
+        type: "divider",
+      },
+      {
+        key: "5",
+        label: "About us",
+      },
+      {
+        key: "6",
+        label: "Help Center",
+      },
+      {
+        key: "7",
+        label: "Settings",
+      },
+    ],
+  },
+];
 
 function HeaderAppPlatform({ setOpen }) {
   const handleHeaderMenu = (key) => {
@@ -97,25 +96,35 @@ function HeaderAppPlatform({ setOpen }) {
   return (
     <>
       <Header style={{ display: "flex", justifyContent: "space-between" }}>
-        <div className="demo-logo" style={{ flex: 1 }}>
-          <h3 style={{ color: "white", fontWeight: "bold" }}>KILOSHARE</h3>
+        <div className="demo-logo">
+          <h3 style={{ color: "white", fontWeight: "bold" }}>KILO-SHARE</h3>
         </div>
-        <Menu
-          theme="dark"
-          mode="horizontal"
-          defaultSelectedKeys={["1"]}
-          items={items}
-          style={{ display: "flex", justifyContent: "center" }}
-          onClick={(value) => handleHeaderMenu(value.key)}
-        />
-        <Menu
-          theme="dark"
-          mode="horizontal"
-        //   defaultSelectedKeys={["2"]}
-          items={items1}
-          style={{ display: "flex", justifyContent: "center" }}
-          onClick={(value) => handleHeaderMenu(value.key)}
-        />
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "end",
+            alignItems: "center",
+            marginLeft: 5
+          }}
+        >
+          <Button
+            size="large"
+            icon={<PlusCircleOutlined />}
+            onClick={() => setOpen(true)}
+            type="primary"
+          >
+            Post a Listing
+          </Button>
+
+          <Menu
+            theme="dark"
+            mode="horizontal"
+            //   defaultSelectedKeys={["2"]}
+            items={items1}
+            // style={{ display: "flex", justifyContent: "center" }}
+            onClick={(value) => handleHeaderMenu(value.key)}
+          />
+        </div>
       </Header>
     </>
   );
